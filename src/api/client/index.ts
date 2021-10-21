1bb70c559a14a38b1e584c85c6aca3acc97155b0
Jayson Custodio <jcustodio@dnamicro.com>
2021-10-21 10:40:33 +0800

    build

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -1,13 +1,10 @@
 import { GraphQLClient } from "graphql-request/dist";
 import { encodeBase64 } from "../utils";
 
-const BASIC_AUTH_USERNAME = "sbc";
-const BASIC_AUTH_PASSWORD = "F0nch3rt0";
-//const GRAPHQL_ENDPOINT_GO_RENTALS = "http://147.182.248.249:4500/graphql";
-const GRAPHQL_ENDPOINT_GO_RENTALS = "http://10.110.3.140:3002/graphql";
-// "https://gorentals-core-api.dnastaging.net/graphql";
-const GRAPHQL_ENDPOINT_PLATFORM =
-  "https://platform-core-api.dnastaging.net/graphql";
+const BASIC_AUTH_USERNAME = "";
+const BASIC_AUTH_PASSWORD = "";
+const GRAPHQL_ENDPOINT_GO_RENTALS = "";
+const GRAPHQL_ENDPOINT_PLATFORM = "";
 
 export const graphql_gorentals = new GraphQLClient(
   GRAPHQL_ENDPOINT_GO_RENTALS,
